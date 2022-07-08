b9ca77b1192e6c9a7b737c5af95bf47e8603510c
Yunus Hoshmand <yunus.hoshmandit@gmail.com>
2022-07-08 08:46:25 +0430

    multiplication table added

---
+++ 1mt.php
@@ -0,0 +1,75 @@
+<?php
+
+$table = 2;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 3;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 4;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 5;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 6;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 7;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 8;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 9;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+$table = 10;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+for($i=1; $i<=$length; $i++)
+echo "$i * $table = ".$i * $table. "<br>";
+
+?>
